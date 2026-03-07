7b13a639bed6b8ee2c51fc8106182ba2663a8ac3
MMC04 <shs047500@gmail.com>
2026-03-07 17:00:39 +0900

    PORT 수정

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`서버 실행중: ${PORT}`);
