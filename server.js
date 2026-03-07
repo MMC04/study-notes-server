57ecd41fbc9c71e554c13f4fa089a2aaee891b3f
MMC04 <shs047500@gmail.com>
2026-03-07 16:54:39 +0900

    undo

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// server.js 맨 위에 추가
-process.on('uncaughtException', (err) => {
-    console.error('uncaughtException:', err);
-});
-
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
