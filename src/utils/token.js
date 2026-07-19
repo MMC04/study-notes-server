fdebba34f925a829b3391177b68e1a987e0e4b91
MMC04 <shs0475@naver.com>
2026-07-19 16:42:12 +0900

    jwt 생성시간 겹침으로 인한 테스트코드 Fail에 대응
    refreshToken 생성시 jti 추가

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,9 +1,18 @@
 // 토큰 생성기
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 function tokenGenerator(user) {
-  const accessToken = jwt.sign({id: user.id}, process.env.ACCESS_SECRET, {expiresIn: '15m'});
-  const refreshToken = jwt.sign({id: user.id}, process.env.REFRESH_SECRET, {expiresIn: '7d'});
+  const accessToken = jwt.sign(
+    { id: user.id },
+    process.env.ACCESS_SECRET,
+    { expiresIn: '15m' }
+  );
+  const refreshToken = jwt.sign(
+    { id: user.id, jti: crypto.randomUUID() },   // ← 매번 고유한 무작위 값 추가
+    process.env.REFRESH_SECRET,
+    { expiresIn: '7d' }
+  );
   return { accessToken, refreshToken };
 }
 
